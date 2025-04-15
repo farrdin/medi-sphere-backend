e75f0cd951aba13cfd222e1c59750db5457f70f9
Fardin Ahmed Alif <fardinahmed.dev@gmail.com>
2025-04-16 00:49:44 +0600

    Project Medi-sphere Initialized

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import Express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+import router from './app/routes'
+
+const app: Application = Express()
+
+//parser
+app.use(Express.json())
+app.use(cors())
+
+// App Api Routes
+app.use('/api', router)
+app.get('/', (req: Request, res: Response) => {
+  res.send('Medi-Sphere App Is Running')
+})
+
+export default app
