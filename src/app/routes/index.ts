4653280f3ab2d2ceaea65e3c76913a8e6aa2987d
Fardin Ahmed Alif <fardinahmed.dev@gmail.com>
2025-04-18 04:05:00 +0600

    Modified

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { medicineRoutes } from '../modules/medicine/medicine.route';
+import authRouter from '../modules/auth/auth.routes';
 const router = Router();
 
 const moduleRoutes = [
@@ -7,10 +8,10 @@ const moduleRoutes = [
     path: '/medicines',
     route: medicineRoutes,
   },
-  // {
-  //   path: '/api/auth',
-  //   route: authRouter,
-  // },
+  {
+    path: '/api/auth',
+    route: authRouter,
+  },
 ];
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
