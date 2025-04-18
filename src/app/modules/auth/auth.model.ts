b52b5209a40e2b8d775615e2ddd0d1a79d36a61c
chowa <arfinchowa524@gmail.com>
2025-04-19 01:21:29 +0600

    authcontroller managed for user role

--- src/app/modules/auth/auth.model.ts
+++ src/app/modules/auth/auth.model.ts
@@ -1,29 +1,37 @@
-import mongoose, { Schema } from "mongoose";
-import bcrypt from "bcryptjs";
-import config from "../../config";
-
-
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcryptjs';
+import config from '../../config';
 
 const UserSchema = new Schema(
   {
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true, minlength: 6},
-    method:{type:String, enum: ["credentials", "github", "google" ], default:"credentials"},
+    password: { type: String, required: true, minlength: 6 },
+    method: {
+      type: String,
+      enum: ['credentials', 'github', 'google'],
+      default: 'credentials',
+    },
     role: {
       type: String,
-      enum: ["user", "admin"],
-      default: "user",
+      enum: ['user', 'admin'],
+      default: 'user',
     },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
-UserSchema.pre('save', async function () {
-    this.password = await bcrypt.hash(
-      this.password,
-      Number(config.bcrypt_salt_rounds),
-    );
-  });
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // only hash if password is new or changed
 
-export const User = mongoose.model("User", UserSchema);
+  try {
+    const saltRounds = Number(config.bcrypt_salt_rounds) || 10;
+    const hashedPassword = await bcrypt.hash(this.password, saltRounds);
+    this.password = hashedPassword;
+    next();
+  } catch (err) {
+    next(err as Error);
+  }
+});
+
+export const User = mongoose.model('User', UserSchema);
